977120ea82ac5bba55058b1fc4a3add305d83889
Milos Stojanovic <milos92s@yahoo.com>
2015-10-31 17:56:34 +0100

    Updated dependancy and added test.

---
+++ src/test/java/rs/fon/eklub/controllers/MembersControllerTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.fon.eklub.controllers;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import rs.fon.eklub.boot.Main;
+
+/**
+ *
+ * @author milos
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class MembersControllerTest {
+    
+    public MembersControllerTest() {
+        
+    }
+    
+}
