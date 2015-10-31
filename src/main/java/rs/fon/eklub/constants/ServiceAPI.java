dece3894008be0699d471870faf306dfabe631c2
Milos Stojanovic <milos92s@yahoo.com>
2015-10-31 16:14:33 +0100

    Adding API path constants.

---
+++ src/main/java/rs/fon/eklub/constants/ServiceAPI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.fon.eklub.constants;
+
+/**
+ *
+ * @author milos
+ */
+public class ServiceAPI {
+    
+    public static final String GET_ALL_MEMBERS = "/members";
+    public static final String GET_MEMBER_BY_ID = "/members/{id}";
+    public static final String POST_SAVE_MEMBER = "/members";
+    public static final String DELETE_MEMBER_BY_ID = "/members/{id}";
+    public static final String GET_SEARCH_MEMBERS = "/members";
+    
+    
+}
